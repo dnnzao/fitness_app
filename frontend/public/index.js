14b6c5b734b568451f7c911a19e2da573df1bc05
Denio Barbosa Junior <deniojr@gmail.com>
2024-02-18 19:26:43 -0300

    First commit

---
+++ frontend/public/index.js
@@ -0,0 +1,8 @@
+/*
+ * Filename: /home/deniojr/Desktop/python_projects/fitness_app/frontend/public/index.js
+ * Path: /home/deniojr/Desktop/python_projects/fitness_app/frontend
+ * Created Date: Sunday, February 18th 2024, 7:21:45 pm
+ * Author: Dênio Barbosa Júnior
+ *
+ * Copyright (c) 2024 Your Company
+ */
